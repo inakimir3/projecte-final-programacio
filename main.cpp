f9f51872404722fabdb68f9b86dc7d61c2381983
rogerbuen <roger.buen@estudiantat.upc.edu>
2025-12-18 17:19:13 +0100

    class cursa

--- main.cpp
+++ main.cpp
@@ -6,7 +6,25 @@ struct Cotxe {
     string nomPilot;
     string escuderia;
     int voltes;
-    enum Estat {BOX, EN_CURSA, FINALITZAT};
+    enum Estat {BOX, EN_CURSA, FINALITZAT, ABANDONAR};
+};
+
+class Cursa {
+private:
+    string nomCircuit;
+    List classificacio;
+public:
+    void afegirCotxe(); 
+    void treureCotxe(); // abandonar, eliminar de la llista
+    void canviEstat(Estat estat) { 
+        if (estat == ABANDONAR){ 
+            treureCotxe();
+        }
+        
+    };
+    void display();
+    // display al reves
+    void adelantar();
 };
 
 int main(){}
